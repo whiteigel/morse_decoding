608e1f6ef9d814c4f52c53c7d033900475d32882
Sergei <whiteigel@gmail.com>
2023-11-28 18:28:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from morse_dict import *
+
+input_string = input('Enter your string:')
+def decode_morse_code(morse_string, input_list):
+    words = input_string.split(' ')
+    decoded_message = ''
+    for word in words:
+        if word in morse_dict.values():
+            decoded_message += list(morse_dict.keys())[list(morse_dict.values()).index(word)]
+        else:
+            decoded_message += ' '
+    return decoded_message
+decoded_message = decode_morse_code(input_string, morse_dict)
+
+print("Decoded Message:", decoded_message)
+
+
+
